d4a54d012b9a1a7eac5a6bada609d8c1c2bd0b6b
Helena <elenakozyreva32343@gmail.com>
2020-09-24 10:55:26 +0300

    create: added reviews section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button__next',
-    prevEl: '.slider-button__prev',
+    nextEl: '.hotel-slider__button__next',
+    prevEl: '.hotel-slider__button__prev',
+  },
+  keyboard: {
+    enabled: true
+  },
+})
+
+var reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button__next',
+    prevEl: '.reviews-slider__button__prev',
   },
   keyboard: {
     enabled: true
