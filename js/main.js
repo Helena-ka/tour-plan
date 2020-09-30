d081bbd292d5bdbc4448750c48e0c56f403dcdb9
Helena <elenakozyreva32343@gmail.com>
2020-09-30 16:43:15 +0300

    create: add mobile menu

--- js/main.js
+++ js/main.js
@@ -23,3 +23,9 @@ var reviewsSlider = new Swiper('.reviews-slider', {
     enabled: true
   },
 })
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню")
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom_visible")
+})
